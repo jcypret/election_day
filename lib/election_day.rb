3a9cfdde61a156004a8a0fa4633574aa6cb48bd7
Justin Cypret <jcypret@gmail.com>
2016-10-15 22:29:08 -0500

    Block out main library methods

--- lib/election_day.rb
+++ lib/election_day.rb
@@ -1,5 +1,44 @@
 require "election_day/version"
 
+# "The Tuesday next after the first Monday in the month of November"
+# -- The 28th Congress (http://memory.loc.gov/ll/llsl/005/0700/07590721.tif)
 module ElectionDay
-  # Your code goes here...
+
+  # Returns `true` if current year is an election year.
+  def self.election_year?
+    Date.today.year.even?
+  end
+
+  # Returns `true` if current year is a midterm election year.
+  def self.midterm_election_year?
+    (Date.today.year - 2014) % 4 == 0
+  end
+
+  # Returns `true` if current year is a presidential election year.
+  def self.presidential_election_year?
+    (Date.today.year - 2012) % 4 == 0
+  end
+
+  # Returns the `Date` of the next election.
+  def self.next_election
+  end
+
+  # Returns the `Date` of the next midterm election.
+  def self.next_midterm_election
+  end
+
+  # Returns the `Date` of the next presidential election.
+  def self.next_presidential_election
+  end
+
+  private
+
+  def self.get_election_date_by_year(year)
+    # start at beginning of November of election year
+    date = Date.new(year, 11, 1)
+    # find the first monday of the month
+    date = date.next_day while !date.monday?
+    # add one more day because election day follows the first Monday
+    date.next_day
+  end
 end
--- spec/election_day_spec.rb
+++ spec/election_day_spec.rb
@@ -1,11 +1,61 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe ElectionDay do
-  it 'has a version number' do
+  it "has a version number" do
     expect(ElectionDay::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "#election_year?" do
+    it "returns true for election years" do
+      Timecop.freeze(Date.new(2016, 7, 4)) do
+        expect(ElectionDay.election_year?).to eq true
+      end
+    end
+
+    it "returns false for non-election years" do
+      Timecop.freeze(Date.new(2017, 7, 4)) do
+        expect(ElectionDay.election_year?).to eq false
+      end
+    end
+  end
+
+  describe "#midterm_election_year?" do
+    it "returns true for midterm election years" do
+      Timecop.freeze(Date.new(2018, 7, 4)) do
+        expect(ElectionDay.midterm_election_year?).to eq true
+      end
+    end
+
+    it "returns false for non-midterm election years" do
+      Timecop.freeze(Date.new(2019, 7, 4)) do
+        expect(ElectionDay.midterm_election_year?).to eq false
+      end
+    end
+
+    it "returns false for presidential election years" do
+      Timecop.freeze(Date.new(2020, 7, 4)) do
+        expect(ElectionDay.midterm_election_year?).to eq false
+      end
+    end
+  end
+
+  describe "#presidential_election_year?" do
+    it "returns true for presidential election years" do
+      Timecop.freeze(Date.new(2016, 7, 4)) do
+        expect(ElectionDay.presidential_election_year?).to eq true
+      end
+    end
+
+    it "returns false for non-election years" do
+      Timecop.freeze(Date.new(2017, 7, 4)) do
+        expect(ElectionDay.presidential_election_year?).to eq false
+      end
+    end
+
+    it "returns false for midterm election years" do
+      Timecop.freeze(Date.new(2018, 7, 4)) do
+        expect(ElectionDay.presidential_election_year?).to eq false
+      end
+    end
   end
 end
